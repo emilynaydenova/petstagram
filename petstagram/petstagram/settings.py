0c636cbc85ffc39f2c06b0754a3b2ef3ba7a2975
@emilynaydenova <emilia_n2@yahoo.com>
2021-10-25 10:34:36 +0300

    Workshop 2

--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'common',
+    'pets',
 ]
 
 MIDDLEWARE = [
@@ -77,9 +78,14 @@ WSGI_APPLICATION = 'petstagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'petstagram',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
+
 }
 
 
